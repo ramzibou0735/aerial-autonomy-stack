461ea98481931d8e2986ee8d6b8ad070006eef56
JacopoPan <jacopo.panerati@utoronto.ca>
2025-07-11 11:23:25 -0400

    added debug mode

--- main.sh
+++ main.sh
@@ -4,12 +4,18 @@
 DRONE_TYPE="${DRONE_TYPE:-quad}" # Options: quad (default), vtol
 AUTOPILOT="${AUTOPILOT:-px4}" # Options: px4 (default), ardupilot
 NUM_DRONES="${NUM_DRONES:-2}" # Number of aircraft (default = 2)
+DEBUG="${DEBUG:-false}" # Whehther to launch the simulation or a simple shell (default = false)
 
 # Grant access to the X server
 xhost +local:docker
 
 # Create network
 docker network create --subnet=42.42.0.0/16 aas-network
+
+ENTRYPOINT_FLAG=""
+if [ "$DEBUG" = "true" ]; then
+  ENTRYPOINT_FLAG="--entrypoint /bin/bash"
+fi
 
 # Launch the simulation container
 gnome-terminal --geometry=120x36+10+10 -- bash -c "echo 'Launching Simulation Container...'; \
@@ -21,6 +27,7 @@ gnome-terminal --geometry=120x36+10+10 -- bash -c "echo 'Launching Simulation Co
     --net=aas-network --ip=42.42.1.99 \
     --privileged \
     --name simulation-container \
+    ${ENTRYPOINT_FLAG} \
     simulation-image; \
   exec bash"
 
@@ -37,6 +44,7 @@ for i in $(seq 1 $NUM_DRONES); do
       --net=aas-network --ip=42.42.1.$i \
       --privileged \
       --name aircraft-container_$i \
+      ${ENTRYPOINT_FLAG} \
       aircraft-image; \
     exec bash"
 done
