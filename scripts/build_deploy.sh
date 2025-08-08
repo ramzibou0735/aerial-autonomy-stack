a056990f9e0fab08d6426f36744d2bf484131344
Jacopo Panerati <jacopo.panerati@utoronto.ca>
2025-08-08 10:46:57 -0400

    moved dockerfile folder

--- scripts/build_deploy.sh
+++ scripts/build_deploy.sh
@@ -6,4 +6,4 @@
 SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 # The first build takes ~1h (mostly to build onnxruntime-gpu from source) and creates an 18GB image
-docker build -t aircraft-image -f "${SCRIPT_DIR}/../docker/Dockerfile.aircraft" "${SCRIPT_DIR}/.."
+docker build -t aircraft-image -f "${SCRIPT_DIR}/docker/Dockerfile.aircraft" "${SCRIPT_DIR}/.."
--- scripts/build_sim.sh
+++ scripts/build_sim.sh
@@ -4,7 +4,7 @@
 SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 # The first build takes ~15' and creates a 19GB image (8GB for ros-humble-desktop with nvidia runtime, 9GB for PX4 and ArduPilot SITL)
-docker build -t simulation-image -f "${SCRIPT_DIR}/../docker/Dockerfile.simulation" "${SCRIPT_DIR}/.."
+docker build -t simulation-image -f "${SCRIPT_DIR}/docker/Dockerfile.simulation" "${SCRIPT_DIR}/.."
 
 # The first build takes ~15' and creates a 16GB image (8GB for ros-humble-desktop with nvidia runtime, 7GB for YOLOv8, ONNX)
-docker build -t aircraft-image -f "${SCRIPT_DIR}/../docker/Dockerfile.aircraft" "${SCRIPT_DIR}/.."
+docker build -t aircraft-image -f "${SCRIPT_DIR}/docker/Dockerfile.aircraft" "${SCRIPT_DIR}/.."
