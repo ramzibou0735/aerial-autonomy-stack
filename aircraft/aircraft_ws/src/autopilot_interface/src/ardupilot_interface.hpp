c0040d56fca64f5998d480dce06fca15dfd8dce5
JacopoPan <jacopo.panerati@utoronto.ca>
2025-08-13 15:54:46 -0400

    mavros examples

--- aircraft/aircraft_ws/src/autopilot_interface/src/ardupilot_interface.hpp
+++ aircraft/aircraft_ws/src/autopilot_interface/src/ardupilot_interface.hpp
@@ -1,5 +1,48 @@
 /*
 
+CLI Tests:
+
+Switch to guided mode
+ros2 service call /mavros/set_mode mavros_msgs/srv/SetMode "{custom_mode: 'GUIDED'}"
+
+Arm (this does not work with the virtual joystick, find a workaround)
+ros2 service call /mavros/cmd/arming mavros_msgs/srv/CommandBool "{value: true}"
+
+Takeoff
+ros2 service call /mavros/cmd/takeoff mavros_msgs/srv/CommandTOL "{altitude: 10.0}"
+
+Reposition
+ros2 topic pub --once /mavros/setpoint_position/local geometry_msgs/msg/PoseStamped '{header: {frame_id: "map"}, pose: {position: {x: 5.0, y: 5.0, z: 10.0}}}'
+
+Velocity reference
+ros2 topic pub --rate 10 --times 50 /mavros/setpoint_velocity/cmd_vel_unstamped geometry_msgs/msg/Twist '{linear: {x: 2.0}}'
+
+Land
+ros2 service call /mavros/cmd/land mavros_msgs/srv/CommandTOL "{}"
+
+Orbit (not working)
+ros2 service call /mavros/cmd/command mavros_msgs/srv/CommandLong "{command: 34, param1: 50.0, param2: 15.0, param3: 0.0, param4: .nan, param5: 45.5477315433, param6: 8.938851933, param7: 250.0}"
+
+Change altitude (not working)
+ros2 service call /mavros/cmd/command mavros_msgs/srv/CommandLong "{command: 186, param1: 250.0, param2: 1}"
+
+Change speed (to do)
+178
+
+Change param (not working)
+ros2 service call /mavros/param/set mavros_msgs/srv/ParamSet '{param_id: "ARMING_CHECK", value: {integer: 0, real: 0.0}}'
+
+VTOL takeoff (not working)
+ros2 service call /mavros/set_mode mavros_msgs/srv/SetMode "{custom_mode: 'QSTABILIZE'}"
+try: ros2 service call /mavros/set_mode mavros_msgs/srv/SetMode "{custom_mode: 'QLOITER'}"
+ros2 service call /mavros/cmd/arming mavros_msgs/srv/CommandBool "{value: true}"
+ros2 service call /mavros/cmd/takeoff mavros_msgs/srv/CommandTOL "{altitude: 40.0}"
+# Push forward at 15 m/s for 10 seconds to ensure a transition
+ros2 topic pub --rate 20 --times 200 /mavros/setpoint_velocity/cmd_vel_unstamped geometry_msgs/msg/Twist '{linear: {x: 15.0}}'
+
+VTOL landing (not tried)
+ros2 service call /mavros/cmd/land mavros_msgs/srv/CommandTOL "{}"
+
 TODO
 
 https://github.dev/mavlink/mavros/blob/ros2/mavros/launch/apm_pluginlists.yaml
